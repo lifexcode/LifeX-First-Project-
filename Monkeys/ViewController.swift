05bb2cee62f30062c53113dcb8622faa055fab03
아 이 딛 <aididhaiqal@yahoo.com>
2016-11-11 15:22:09 +0800

    Initial Commit

---
+++ Monkeys/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Monkeys
+//
+//  Created by Aiman Hazimin on 11/11/2016.
+//  Copyright © 2016 Aidid Haiqal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
