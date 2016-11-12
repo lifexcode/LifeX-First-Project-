f7010c2f12c3c7f3f56a25770c5a15f1f9d2474a
아 이 딛 <aididhaiqal@yahoo.com>
2016-11-12 14:43:40 +0800

    My first commit

--- Monkeys/ViewController.swift
+++ Monkeys/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var momoLabel: UILabel!
+    
+var tapCount = 0
+    
+    @IBAction func buttontapped(_ sender: Any) { tapCount = tapCount + 1
+        if tapCount >= 5 {
+            momoLabel.text = "Syahir!!!"
+        }
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
